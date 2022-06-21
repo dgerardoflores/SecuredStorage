27cd3ddd82a48d6a9bda3ff1347c93ecff307df3
Gerardo Flores <30551778+gerardofloresdev@users.noreply.github.com>
2022-06-20 22:59:25 -0600

    Add set to Storage Utils

--- src/helpers/Storage.js
+++ src/helpers/Storage.js
@@ -1,5 +1,36 @@
+import TypesStorages from './../interfaces/TypesStorages.js';
 class Storage {
-    
+    static set(prefix, key, value, type) {
+        const newKey = Storage.generateKeyWithPrefix(prefix, key);
+
+        switch (type) {
+            case TypesStorages.LOCAL_STORAGE:
+                localStorage.setItem(newKey, JSON.stringify(value));
+
+                break;
+            case TypesStorages.SESSION_STORAGE:
+                sessionStorage.setItem(newKey, JSON.stringify(value));
+
+                break;
+            default:
+                throw new Error('An error occurred while choosing the correct storage')
+        }
+    }
+
+    static get(prefix, key, type) {
+        switch (type) {
+            case TypesStorages.LOCAL_STORAGE:
+                break;
+            case TypesStorages.SESSION_STORAGE:
+                break;
+            default:
+                throw new Error('An error occurred while choosing the correct storage')
+        }
+    }
+
+    static generateKeyWithPrefix(prefix, key) {
+        return prefix + '-' + key;
+    }
 }
 
 export default Storage;
---
+++ src/interfaces/TypesStorages.js
@@ -0,0 +1,6 @@
+const TypesStorages = {
+    LOCAL_STORAGE: 'localStorage',
+    SESSION_STORAGE: 'sessionStorage',
+};
+
+export default Object.freeze(TypesStorages);
